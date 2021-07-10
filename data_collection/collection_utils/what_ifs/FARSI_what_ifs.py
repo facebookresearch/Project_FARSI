5b74c6232f2271af668be826000d12a82df260cc
Behzad Boroujerdian <behzadboro@fb.com>
2021-07-10 01:25:03 -0400

    frequncy of 300,500 does not allow hpvm-cava to meet. changed it back)

--- data_collection/collection_utils/what_ifs/FARSI_what_ifs.py
+++ data_collection/collection_utils/what_ifs/FARSI_what_ifs.py
@@ -446,11 +446,11 @@ if __name__ == "__main__":
     # set the workload
 
     workloads = {"edge_detection"}
-    #workloads = {"hpvm_cava"}
+    workloads = {"hpvm_cava"}
     #workloads = {"audio_decoder"}
     #workloads = {"SLAM"}
-    workloads ={"audio_decoder", "edge_detection", "hpvm_cava"}
-    #workloads ={"audio_decoder", "edge_detection"}
+    #workloads ={"audio_decoder", "edge_detection", "hpvm_cava"}
+    workloads ={"audio_decoder", "edge_detection"}
 
     #workloads = {"partial_SOC_example_hard"}
     #workloads = {"simple_all_parallel"}
@@ -460,9 +460,9 @@ if __name__ == "__main__":
     #ip_freq_range = {"incr":3, "upper_bound":8}
     #mem_freq_range = {"incr":3, "upper_bound":6}
     #ic_freq_range = {"incr":4, "upper_bound":6}
-    ip_freq_range = [1,3,5]
-    mem_freq_range = [1,3,5]
-    ic_freq_range = [1,3,5]
+    ip_freq_range = [1,4,6,8]
+    mem_freq_range = [1,4,6,8]
+    ic_freq_range = [1,4,6,8]
     tech_node_SF = {"perf":1, "energy":.064, "area":.037}   # technology node scaling factor
     db_population_misc_knobs = {"ip_freq_correction_ratio": 1, "gpp_freq_correction_ratio": 1,
                                 "ip_spawn": {"ip_loop_unrolling": ip_loop_unrolling, "ip_freq_range": ip_freq_range},
