4e62f79136e9dac8757701ddee11a7688e48f7ed
yingj4 <yingj4@illinois.edu>
2021-07-20 19:58:48 -0500

    Update for Ying's directory

--- settings/config_plotting.py
+++ settings/config_plotting.py
@@ -1,6 +1,6 @@
 zoneNum = 4
 #run_folder_name = "/Users/behzadboro/Project_FARSI_dir/Project_FARSI_with_channels/data_collection/data/simple_run/07-18_13-49_22/"
-run_folder_name = ""
-top_result_folder = "/Users/behzadboro/Project_FARSI_dir/Project_FARSI_with_channels/data_collection/data/simple_run/"
+run_folder_name = "/home/yingj4/Desktop/FARSI_results/07-19_19-43_53/"
+top_result_folder = "/home/yingj4/Desktop/FARSI_results/07-19_19-43_53/"
 ignore_file_names = ["README.md", "figures", "cross_workloads"]
 plot_list = ["cross_workloads"]
