7a92a0af1de21628c72fbc3a8eb09b20d232473c
yingj4 <yingj4@illinois.edu>
2021-07-25 14:29:16 -0500

    blah

--- settings/config_plotting.py
+++ settings/config_plotting.py
@@ -1,6 +1,6 @@
 zoneNum = 4
 #run_folder_name = "/Users/behzadboro/Project_FARSI_dir/Project_FARSI_with_channels/data_collection/data/simple_run/07-18_13-49_22/"
-run_folder_name = ""
-top_result_folder = "/Users/behzadboro/Project_FARSI_dir/Project_FARSI_with_channels/data_collection/data/simple_run/"
+run_folder_name = "/home/yingj4/Desktop/FARSI_results/arch-aware-vs-random"
+top_result_folder = "/home/yingj4/Desktop/FARSI_results/arch-aware-vs-random"
 ignore_file_names = ["README.md", "figures", "cross_workloads", "single_workload"]
 plot_list = ["cross_workloads", "single_workload"]
